7c2e1258cd57d240296d08b46fba5fb38a6a0312
John Duprey <john.duprey@iduprey.com>
2024-11-25 00:28:38 -0500

    badge tweaks..

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='your-package-name',
+    version='0.1.0',  # Update this version number
+    packages=find_packages(),
+    // ...existing code...
+)
